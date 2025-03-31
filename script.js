5f836ca0b49eaec669ef6a1b8739cde88054d893
Harsha <Harshagowdasv02@gmail.com>
2025-03-31 19:21:33 +0530

    added

---
+++ script.js
@@ -0,0 +1,16 @@
+function updateClock(){
+    let now = new Date();
+    let hours = now.getHours().toString().padStart(2,'0');
+    let minutes = now.getMinutes().toString().padStart(2,'0');
+    let seconds = now.getSeconds().toString().padStart(2,'0');
+
+    document.getElementById("clock").textContent= `${hours}:${minutes}:${seconds}`;
+    
+}
+
+setInterval(updateClock,1000);
+updateClock();
+
+document.getElementById("toggle").addEventListener("click",()=>{
+    document.body.classList.toggle("dark-theme")
+});
